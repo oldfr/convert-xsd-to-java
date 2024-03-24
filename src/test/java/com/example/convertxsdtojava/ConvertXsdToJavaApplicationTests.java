dcb9c4323c626cbed5c2a6140a2695bebf89c63f
oldfr <ankitha.codes@gmail.com>
2024-03-24 20:15:44 +0530

    initial commit to convert pmml 4.4 .xsd filw to JAVA classes

---
+++ src/test/java/com/example/convertxsdtojava/ConvertXsdToJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convertxsdtojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertXsdToJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
